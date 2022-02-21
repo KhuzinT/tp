92efd87d2801e74ace0aa2f7332a1b6acac77995
KhuzinT <KhuzinTka@yandex.ru>
2022-02-21 23:47:57 +0300

    writing code

--- run.sh
+++ run.sh
@@ -4,3 +4,12 @@ absolute_path = $1
 expansion = $2
 name_folder = $3
 name_archive = $4
+
+tmp_str = "*."
+str = "$tmp_str$expansion"
+
+mkdir name_folder
+
+find absolute_path -name str -exec cp --backup=numbered {}./name_folder 
+
+tar -zcf name_archive ./name_folder
